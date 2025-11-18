e0a429b3e9dea01439b3d6c489898d5f6b775e6f
jayuhl <juhl@pascack.org>
2025-11-17 20:16:26 -0500

    Update image and demo URLs for projects

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ const projects = [
     {
         title: "Project 1: Tic-Tac-Toe App",
         description: "A Python tool that scans websites for broken links and security vulnerabilities. Used libraries like Requests and BeautifulSoup.",
-        imageUrl: "images/TicTacToeapp_image.png", // Use a placeholder or a path like "images/bopit.png"
+        imageUrl: "../images/TicTacToeapp_image.png", // Use a placeholder or a path like "images/bopit.png"
         demoUrl: "../app_pages/mastermind.html", // Link to your live demo
         repoUrl: "#"  // Link to your GitHub repository
     },
     {
         title: "Project 2: Odometer App",
         description: "A web-based tool built with JavaScript, HTML, and CSS that animates different sorting algorithms like Bubble Sort and Quick Sort.",
-        imageUrl: "images/odometerapp_image.png",
+        imageUrl: "../images/odometerapp_image.png",
         demoUrl: "#",
         repoUrl: "#"
     },
     {
         title: "Project 3: BopIt App",
         description: "A description for a third project. You can add as many as you like! Just copy this format.",
-        imageUrl: "images/bopitapp_image.png",
-        demoUrl: "../app_pages/BopIt.html",
+        imageUrl: "../images/bopitapp_image.png",
+        demoUrl: "../pages/BopIt.html",
         repoUrl: "#"
     }
     // Add more projects here...
